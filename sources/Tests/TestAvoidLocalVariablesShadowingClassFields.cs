2f4c4e23c2a8ad44c6578ddd8480de241f6c484c
cgu <c.gufflet@castsoftware.com>
2020-12-18 13:36:59 +0100

    EINETRULES-1: Reorganizing source folders to make difference with resources to be delivered in nupkg

---
+++ sources/Tests/TestAvoidLocalVariablesShadowingClassFields.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CastDotNetExtension;
+
+namespace UnitTests.UnitTest {
+   [TestFixture]
+   class TestAvoidLocalVariablesShadowingClassFields {
+      [Test]
+      public void Test() {
+
+         /// [TODO#9] load the source code to test from resources
+         // note: a source file should have been added to the resources using the Resource file "SourcesToTest.resx"
+         var testSrc = UnitTests.Properties.SourcesToTest.AvoidLocalVariablesShadowingClassFields_Source;
+
+         /// [TODO#10] create the checker object, parametrized with the type of the QR to test
+         var checker = CastDotNetExtensionChecker<AvoidLocalVariablesShadowingClassFields>.CreateInstance();
+         Assert.IsTrue(checker != null);
+
+
+         /// [TODO#11] setup the expected bookmarks
+         /// [TODO#12] launch the processing on the given source code
+         checker
+            //.AddSource(@"C:\Sources\tools.cs")
+            //.AddAssemblyRef(@"C:\packages\assembly.dll")
+             .Apply(testSrc);
+
+         checker
+            .AddExpected(12, 16)
+            .AddExpected(17, 19)
+            .AddExpected(36, 16)
+            .AddExpected(40, 16)
+            .AddExpected(54, 19)
+            .AddExpected(62, 19)
+            .AddExpected(69, 19)
+            .Validate();
+
+         /// [TODO#13] Check the results
+         Assert.IsTrue(checker.IsValid(), checker.getStatus());
+
+         //Assert.IsFalse(checker.ResultsMissing.Any());
+         //Assert.IsFalse(checker.ResultsUnexpected.Any());
+
+      }
+
+   }
+}
---
+++ sources/Tests/TestSources/AvoidLocalVariablesShadowingClassFields_Source.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.UnitTest.Sources {
+   class AvoidLocalVariablesShadowingClassFields_Source {
+      class Shadow {
+         private int aMember;
+
+         void Shadow_aMemberKO() {
+            int aMember = 0;
+         }
+
+         void Shadow_aMemberInInnerScopeKO() {
+            {
+               int aMember = 0;
+            }
+         }
+
+         void DontShadow_aMemberOK() {
+            {
+               int aMemberX = 0;
+            }
+         }
+
+         protected int aProtectedMemberOK = 0;
+         protected int aProtectedMemberKO = 0;
+      }
+
+
+      class AnotherShadow : Shadow {
+         protected int aProtectedMemberOK = 0;
+
+         public AnotherShadow() {
+            int aProtectedMemberKO = 0;
+         }
+
+         public void ShadowBase_aProtectedMemberKO() {
+            int aProtectedMemberKO = 0;
+         }
+
+         public void TryShadowBasePrivateMemberOK() {
+            int aMember = 0;
+         }
+
+         class InnerShadow {
+            int aInnerShadowMember = 0;
+            public InnerShadow() {
+               int aProtectedMemberOK = 0;
+            }
+
+            void Shadow_aInnerShadowMember() {
+               int aInnerShadowMember = 0;
+            }
+
+            public int aPublicInnerShadowMember = 0;
+         }
+
+         class InnerShadowAnother : InnerShadow {
+            public InnerShadowAnother() {
+               int aPublicInnerShadowMember = 0;
+            }
+         }
+
+         struct AStruct {
+            private int aStructMember;
+            public void Shadow() {
+               int aStructMember = 0;
+            }
+         }
+
+      }
+   }
+}
