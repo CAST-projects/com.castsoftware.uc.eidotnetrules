bf3217652d952ecc0a2815185b0d9112ce1ff4fe
ero <e.roy@castsoftware.com>
2020-12-21 18:07:38 +0100

    EINETRULES-1: add build part

--- build/build_and_package.bat
+++ build/build_and_package.bat
@@ -140,7 +140,7 @@ echo Preparing extension folder
 echo ==============================================
 robocopy /mir %SRCDIR%\nuget\package_files %RESDIR%
 if errorlevel 8 goto endclean
-robocopy %RELEASE64% %RESDIR% *.* /xf *.pdb /xf *test*.*
+robocopy /s %RELEASE64% %RESDIR% *.* /xf *.pdb /xf *test*.*
 if errorlevel 8 goto endclean
 
 pushd %WKSP%
