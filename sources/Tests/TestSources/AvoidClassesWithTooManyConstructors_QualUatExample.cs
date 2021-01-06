5c302043767504cc3f7135b9dd9f907022564925
vmu <v.mushrif@castsoftware.com>
2021-01-06 17:31:25 +0530

    EINETRULES-7 EINETRULES-6 EINETRULES-5 EINETRULES-4 EINETRULES-3 EINETRULES-2

--- sources/Tests/TestSources/AvoidClassesWithTooManyConstructors_QualUatExample.cs
+++ sources/Tests/TestSources/AvoidClassesWithTooManyConstructors_QualUatExample.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text;   
 using System.Threading.Tasks;
 
 namespace Sonar.Analyzers.CSharp.Common.Tests.UnitTests.Analyzers.AvoidClassesWithTooManyConstructors
