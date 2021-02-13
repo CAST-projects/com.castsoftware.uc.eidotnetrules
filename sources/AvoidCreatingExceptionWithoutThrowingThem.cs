36beb7527c56b2de097bfffb04ab6ed955cb889a
vmu <v.mushrif@castsoftware.com>
2021-02-14 00:00:01 +0530

    EINETRULES-21: Rewrite: Fix performance issue

--- sources/AvoidCreatingExceptionWithoutThrowingThem.cs
+++ sources/AvoidCreatingExceptionWithoutThrowingThem.cs
@@ -33,7 +33,7 @@ namespace CastDotNetExtension {
         /// </summary>
         /// <param name="context"></param>
         public override void Init(AnalysisContext context) {
-         context.RegisterSyntaxNodeAction(this.Analyze, SyntaxKind.ObjectCreationExpression, SyntaxKind.ThrowStatement);
+         //context.RegisterSyntaxNodeAction(this.Analyze, SyntaxKind.ObjectCreationExpression, SyntaxKind.ThrowStatement);
          context.RegisterSemanticModelAction(this.SemenaticModelAnalysisEnd);
       }
 
@@ -71,6 +71,7 @@ namespace CastDotNetExtension {
             try {
                var newSyntax = context.Node as ObjectCreationExpressionSyntax;
                if (null != newSyntax) {
+                  var typeSymbol = context.SemanticModel.GetTypeInfo(context.Node).Type;
                   if (!_exceptionsThrown.Contains(newSyntax)) {
                      var typeName = newSyntax.Type;
                      if (null != typeName) {
@@ -83,12 +84,13 @@ namespace CastDotNetExtension {
                                  if (newSyntax.Parent.Parent is VariableDeclaratorSyntax) {
                                     var variableDeclarator = newSyntax.Parent.Parent as VariableDeclaratorSyntax;
                                     var varName = variableDeclarator.Identifier.ToString();
+                                    ISymbol sym = context.SemanticModel.GetDeclaredSymbol(variableDeclarator);
                                     Dictionary<SyntaxNode, ISymbol> nodes = null;
                                     if (!_exceptionsVars.TryGetValue(varName, out nodes)) {
                                        nodes = new Dictionary<SyntaxNode, ISymbol>();
                                        _exceptionsVars[varName] = nodes;
                                     }
-                                    nodes[newSyntax.Parent.Parent] = context.ContainingSymbol;
+                                    nodes[variableDeclarator] = context.ContainingSymbol;
                                  }
                               }
                               else {
@@ -118,6 +120,7 @@ namespace CastDotNetExtension {
                      else {
                         if (null != throwStatement.Expression && throwStatement.Expression is IdentifierNameSyntax) {
                            var identifierNameSyntax = throwStatement.Expression as IdentifierNameSyntax;
+                           var sym = context.SemanticModel.GetSymbolInfo(identifierNameSyntax);
                            if (_exceptionsVars.Keys.Contains(identifierNameSyntax.Identifier.ToString())) {
                               _exceptionsVars.Remove(identifierNameSyntax.Identifier.ToString());
                            }
@@ -133,32 +136,110 @@ namespace CastDotNetExtension {
 
       }
 
+      private void AnalyzeCreation(ObjectCreationExpressionSyntax creation, SemanticModel semanticModel,
+         INamedTypeSymbol systemException, INamedTypeSymbol systemObject, ref HashSet<ISymbol> exceptionVars, 
+         ref HashSet<SyntaxNode> violatingNodes) {
+         if (null != creation) {
+            var typeSymbol = semanticModel.GetTypeInfo(creation).Type;
+            int line = creation.GetLocation().GetMappedLineSpan().Span.Start.Line;
+            
+            if (null != typeSymbol && typeSymbol is INamedTypeSymbol) {
+               if (IsException(typeSymbol as INamedTypeSymbol, systemException, systemObject)) {
+                  if (creation.Parent is EqualsValueClauseSyntax) {
+                     if (creation.Parent.Parent is VariableDeclaratorSyntax) {
+                        var variableDeclarator = creation.Parent.Parent as VariableDeclaratorSyntax;
+                        ISymbol sym = semanticModel.GetDeclaredSymbol(variableDeclarator);
+                        if (null != sym) {
+                           exceptionVars.Add(sym);
+                        }
+                     }
+                  } else if (creation.Parent is ExpressionStatementSyntax) {
+                     violatingNodes.Add(creation);
+                  }
+               }
+            }
+         }
+      }
+
+      private void AnalyzeThrow(ThrowStatementSyntax aThrow, SemanticModel semanticModel, ref HashSet<ISymbol> exceptionVars) {
+            if (null != aThrow) {
+               var identifier = aThrow.Expression as IdentifierNameSyntax;
+               if (null != identifier) {
+                  var symbol = semanticModel.GetSymbolInfo(identifier).Symbol;
+                  if (null != symbol && exceptionVars.Contains(symbol)) {
+                     exceptionVars.Remove(symbol);
+                  }
+               }
+            }
+      }
+
+
       private void SemenaticModelAnalysisEnd(SemanticModelAnalysisContext context) {
          lock (_lock) {
             try {
                if ("C#" == context.SemanticModel.Compilation.Language) {
-                  foreach (var exceptions in _exceptionsVars.Values) {
-                     foreach (SyntaxNode exception in exceptions.Keys) {
-                        var pos = exception.SyntaxTree.GetMappedLineSpan(exception.Span);
-                        //Log.Warn(pos.ToString());
-                        AddViolation(exceptions[exception], new FileLinePositionSpan[] { pos });
+
+                  IEnumerable<SyntaxNode> nodes = 
+                     context.SemanticModel.SyntaxTree.GetRoot().DescendantNodes().Where(n => (
+                        (n.IsKind(SyntaxKind.ObjectCreationExpression) && n.Parent.Kind() != SyntaxKind.ThrowStatement) ||
+                        (n.IsKind(SyntaxKind.ThrowStatement) && SyntaxKind.IdentifierName == (n as ThrowStatementSyntax).Expression.Kind())
+                        ));
+
+                  INamedTypeSymbol systemException = context.SemanticModel.Compilation.GetTypeByMetadataName("System.Exception");
+                  INamedTypeSymbol systemObject = context.SemanticModel.Compilation.GetTypeByMetadataName("System.Object");
+
+                  var exceptionVars = new HashSet<ISymbol>();
+                  var violatingNodes = new HashSet<SyntaxNode>();
+                  foreach (var node in nodes) {
+                     if (node.IsKind(SyntaxKind.ObjectCreationExpression)) {
+                        AnalyzeCreation(node as ObjectCreationExpressionSyntax, context.SemanticModel, systemException, systemObject, ref exceptionVars, ref violatingNodes);
+                     } else {
+                        AnalyzeThrow(node as ThrowStatementSyntax, context.SemanticModel, ref exceptionVars);
                      }
                   }
 
-                  foreach (SyntaxNode exception in _exceptionsNotThrown.Keys) {
-                     var pos = exception.SyntaxTree.GetMappedLineSpan(exception.Span);
-                     //Log.Warn(pos.ToString());
-                     AddViolation(_exceptionsNotThrown[exception], new FileLinePositionSpan[] { pos });
+                  ISymbol violatingSymbol = null;
+                  foreach (var exceptionVar in exceptionVars) {
+                     
+                     if (exceptionVar is IFieldSymbol) {
+                        violatingSymbol = exceptionVar;
+                     } else {
+                        violatingSymbol = exceptionVar.ContainingSymbol;
+                     }
+
+                     var pos = exceptionVar.Locations.FirstOrDefault().GetMappedLineSpan();
+                     AddViolation(violatingSymbol, new FileLinePositionSpan[] { pos });
                   }
 
-                  Log.Info("_exceptionsNotThrown: " + _exceptionsNotThrown.Count);
-                  Log.Info("_exceptionsThrown: " + _exceptionsThrown.Count);
-                  Log.Info("_exceptionsVars: " + _exceptionsVars.Count);
-                  Log.Info("_typeToIsException: " + _typeToIsException.Count);
-                  _exceptionsNotThrown.Clear();
-                  _exceptionsThrown.Clear();
-                  _exceptionsVars.Clear();
-                  _typeToIsException.Clear();
+                  foreach (var node in violatingNodes) {
+                     violatingSymbol = context.SemanticModel.GetEnclosingSymbol(node.Span.Start);
+                     if (null != violatingSymbol) {
+                        AddViolation(violatingSymbol, new FileLinePositionSpan[] { node.GetLocation().GetMappedLineSpan() });
+                     }
+                  }
+
+                  //foreach (var exceptions in _exceptionsVars.Values) {
+                  //   foreach (SyntaxNode exception in exceptions.Keys) {
+                  //      var pos = exception.SyntaxTree.GetMappedLineSpan(exception.Span);
+                  //      //Log.Warn(pos.ToString());
+                  //      AddViolation(exceptions[exception], new FileLinePositionSpan[] { pos });
+                  //   }
+                  //}
+
+                  //foreach (SyntaxNode exception in _exceptionsNotThrown.Keys) {
+                  //   var pos = exception.SyntaxTree.GetMappedLineSpan(exception.Span);
+                  //   //Log.Warn(pos.ToString());
+                  //   AddViolation(_exceptionsNotThrown[exception], new FileLinePositionSpan[] { pos });
+                  //}
+
+                  //Log.Info("_exceptionsNotThrown: " + _exceptionsNotThrown.Count);
+                  //Log.Info("_exceptionsThrown: " + _exceptionsThrown.Count);
+                  //Log.Info("_exceptionsVars: " + _exceptionsVars.Count);
+                  //Log.Info("_typeToIsException: " + _typeToIsException.Count);
+                  //_exceptionsNotThrown.Clear();
+                  //_exceptionsThrown.Clear();
+                  //_exceptionsVars.Clear();
+                  //_typeToIsException.Clear();
                }
 
             }
