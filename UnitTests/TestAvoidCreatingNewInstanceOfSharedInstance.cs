7dd5f365e5754accf9f1ad6c2d6ae0e52388a05e
Ludovic Baillet <l.baillet@castsoftware.com>
2022-07-22 10:44:27 +0200

    EINETRULES-86 - Fix unit tests for linux

--- UnitTests/TestAvoidCreatingNewInstanceOfSharedInstance.cs
+++ UnitTests/TestAvoidCreatingNewInstanceOfSharedInstance.cs
@@ -4,42 +4,57 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using CastDotNetExtension;
+using System.IO;
+using System.Reflection;
 
-namespace UnitTests.UnitTest {
-   [TestFixture]
-   class TestAvoidCreatingNewInstanceOfSharedInstance {
+namespace UnitTests.UnitTest
+{
+    [TestFixture]
+    class TestAvoidCreatingNewInstanceOfSharedInstance
+    {
 
-      [Test]
-      public void Test_operations() {
-         var testSrc = UnitTests.Properties.SourcesToTest.AvoidCreatingNewInstanceOfSharedInstance_Source;
-         
-         var checker = CastDotNetExtensionChecker<AvoidCreatingNewInstanceOfSharedInstance>.CreateInstance()
-            ;
+        public static DirectoryInfo GetExecutingDirectory()
+        {
+            var location = new Uri(Assembly.GetCallingAssembly().GetName().CodeBase);
+            var path = Uri.UnescapeDataString(location.AbsolutePath);
+            return new FileInfo(path).Directory;
+        }
 
-         checker = checker
-            .AddAssemblyRef(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.dll")
-            .AddAssemblyRef(@"c:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\Facades\System.ComponentModel.TypeConverter.dll")
-            .AddAssemblyRef(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.ComponentModel.Composition.dll")
-         ;
+        [Test]
+        public void Test_operations()
+        {
+            var testSrc = UnitTests.Properties.SourcesToTest.AvoidCreatingNewInstanceOfSharedInstance_Source;
 
-         Assert.IsTrue(checker != null);
+            var checker = CastDotNetExtensionChecker<AvoidCreatingNewInstanceOfSharedInstance>.CreateInstance()
+               ;
 
-         checker
-             .Apply(testSrc);
+            var pathPackagesUnitTests = Path.Combine(GetExecutingDirectory().FullName, "PackagesUnitTests");
 
-         checker
-            .AddExpected(112,20)
-            .AddExpected(90,25)
-            .AddExpected(83,31)
-            .AddExpected(77,22)            
-            .Validate();
+            // 3 assemblies copied from @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\" folder
+            checker = checker
+                .AddAssemblyRef(Path.Combine(pathPackagesUnitTests, "System.dll"))
+                .AddAssemblyRef(Path.Combine(pathPackagesUnitTests, "Facades", "System.ComponentModel.TypeConverter.dll"))
+                .AddAssemblyRef(Path.Combine(pathPackagesUnitTests, "System.ComponentModel.Composition.dll"))
+             ;
 
-         Assert.IsTrue(checker.IsValid(), checker.GetStatus());
+            Assert.IsTrue(checker != null);
 
-         //Assert.IsFalse(checker.ResultsMissing.Any());
-         //Assert.IsFalse(checker.ResultsUnexpected.Any());
+            checker
+                .Apply(testSrc);
 
-      }
+            checker
+               .AddExpected(112, 20)
+               .AddExpected(90, 25)
+               .AddExpected(83, 31)
+               .AddExpected(77, 22)
+               .Validate();
 
-   }
+            Assert.IsTrue(checker.IsValid(), checker.GetStatus());
+
+            //Assert.IsFalse(checker.ResultsMissing.Any());
+            //Assert.IsFalse(checker.ResultsUnexpected.Any());
+
+        }
+
+    }
 }
