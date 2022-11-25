c5d973dda4bb910ed0bc1efade271f1fa84e6574
tcr <t.crestaux@castsoftware.com>
2022-11-25 08:44:03 +0100

    EINETRULES-93: copy test assemblies in build folder

--- sources/Tests/TestSources/AvoidUnsafeObjectBinding_Source.cs
+++ sources/Tests/TestSources/AvoidUnsafeObjectBinding_Source.cs
@@ -10,227 +10,227 @@ using System.ComponentModel;
 
 namespace UnitTests.UnitTest.Sources
 {
-    //public class AvoidUnsafeObjectBinding_Source
-    //{
+    public class AvoidUnsafeObjectBinding_Source
+    {
 
-    //}
+    }
 
-    //public class UserDbcontext : DbContext
-    //{
+    public class UserDbcontext : DbContext
+    {
 
-    //    public DbSet<User> Users { get; set; }
-    //    public DbSet<string> Names { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<string> Names { get; set; }
 
-    //}
+    }
 
-    //public class UserController : Controller
-    //{
-    //    private UserDbcontext _db = new UserDbcontext();
-    //    [HttpPost]
-    //    public ActionResult Create(User user) // Violation
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-    //            _db.Users.Add(user);
-    //            _db.SaveChanges();
-    //            return RedirectToAction("Index");
-    //        }
-    //        return View();
-    //    }
+    public class UserController : Controller
+    {
+        private UserDbcontext _db = new UserDbcontext();
+        [HttpPost]
+        public ActionResult Create(User user) // Violation
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Users.Add(user);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
 
-    //    public ActionResult UpdateUser(User user) // No Violation
-    //    {
-    //        return View();
-    //    }
+        public ActionResult UpdateUser(User user) // No Violation
+        {
+            return View();
+        }
 
-    //    [HttpPost]
-    //    [ValidateAntiForgeryToken]
-    //    public async Task<ActionResult> UpdateUserAccount(User user) //Violation
-    //    {
-    //        if (!ModelState.IsValid)
-    //        {
-    //            return View();
-    //        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> UpdateUserAccount(User user) //Violation
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
 
-    //        try
-    //        {
-    //            using (UserDbcontext db = new UserDbcontext())
-    //            {
-    //                db.Users.Attach(user);
-    //                await db.SaveChangesAsync();
-    //                return View("Index");
-    //            }
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            ModelState.AddModelError("Error when saving data", ex);
-    //            return View();
-    //        }
-    //    }
+            try
+            {
+                using (UserDbcontext db = new UserDbcontext())
+                {
+                    db.Users.Attach(user);
+                    await db.SaveChangesAsync();
+                    return View("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Error when saving data", ex);
+                return View();
+            }
+        }
 
-    //    [HttpPost]
-    //    public ActionResult Create2(UserDto userDto) // No Violation
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-    //            User user = new User();
-    //            user.UserName = userDto.UserName;
-    //            user.UserAge = userDto.UserAge;
-    //            _db.Users.Add(user);
-    //            _db.SaveChanges();
-    //            return RedirectToAction("Index");
-    //        }
-    //        return View();
-    //    }
+        [HttpPost]
+        public ActionResult Create2(UserDto userDto) // No Violation
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User();
+                user.UserName = userDto.UserName;
+                user.UserAge = userDto.UserAge;
+                _db.Users.Add(user);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
 
-    //    [HttpPost]
-    //    public ActionResult Edit()  //Violation
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-    //            var user = new User();
-    //            TryUpdateModel(user);
-    //            _db.Users.Add(user);
-    //            _db.SaveChanges();
-    //            return RedirectToAction("Index");
-    //        }
-    //        return View();
-    //    }
+        [HttpPost]
+        public ActionResult Edit()  //Violation
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User();
+                TryUpdateModel(user);
+                _db.Users.Add(user);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
 
-    //    [HttpPost]
-    //    public ActionResult Edit2()  //Violation
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-    //            var user = new User();
-    //            UpdateModel(user);
-    //            _db.Users.Add(user);
-    //            _db.SaveChanges();
-    //            return RedirectToAction("Index");
-    //        }
-    //        return View();
-    //    }
-    //}
+        [HttpPost]
+        public ActionResult Edit2()  //Violation
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User();
+                UpdateModel(user);
+                _db.Users.Add(user);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
 
-    //public class User
-    //{
-    //    public string UserName { get; set; }
-    //    public string Password { get; set; }
-    //    public int UserAge { get; set; }
-    //    public int UserId { get; set; }
-    //    public bool IsAdmin { get; set; }
-    //}
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int UserAge { get; set; }
+        public int UserId { get; set; }
+        public bool IsAdmin { get; set; }
+    }
 
-    //public class UserDto
-    //{
-    //    public string UserName { get; set; }
-    //    public int UserAge { get; set; }
-    //}
+    public class UserDto
+    {
+        public string UserName { get; set; }
+        public int UserAge { get; set; }
+    }
 
-    //public class EmployeeController : Controller
-    //{
-    //    private EmployeeDbcontext _db = new EmployeeDbcontext();
+    public class EmployeeController : Controller
+    {
+        private EmployeeDbcontext _db = new EmployeeDbcontext();
 
-    //    [HttpPost]
-    //    [ActionName("Edit")]
-    //    public ActionResult Edit_Post(int id)
-    //    {
-    //        var employee = _db.Employees.Where(_ => _.ID == id).FirstOrDefault();
-    //        if (employee != null)
-    //        {
-    //            UpdateModel<IEmployee>(employee); // No violation
-    //            if (ModelState.IsValid)
-    //            {
-    //                _db.SaveChanges();
-    //                return RedirectToAction("Index");
-    //            }
-    //        }
-    //        return View();
-    //    }
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult Edit_Post(int id)
+        {
+            var employee = _db.Employees.Where(_ => _.ID == id).FirstOrDefault();
+            if (employee != null)
+            {
+                UpdateModel<IEmployee>(employee); // No violation
+                if (ModelState.IsValid)
+                {
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
 
-    //    [HttpPost]
-    //    [ActionName("Create")]
-    //    public ActionResult Create_Post([Bind(Include = "Gender, City, Salary, DateOfBirth")] Employee employee) // No violation
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-    //            _db.Employees.Add(employee);
-    //            _db.SaveChanges();
-    //            return RedirectToAction("Index");
-    //        }
-    //        return View();
-    //    }
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create_Post([Bind(Include = "Gender, City, Salary, DateOfBirth")] Employee employee) // No violation
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Employees.Add(employee);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
 
-    //    [HttpPost]
-    //    [ActionName("Create2")]
-    //    public ActionResult Create2_Post() // No violation
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-    //            var employee = new Employee();
-    //            TryUpdateModel(employee, includeProperties: new[] { "Gender, City, Salary, DateOfBirth" });
-    //            _db.Employees.Add(employee);
-    //            _db.SaveChanges();
-    //            return RedirectToAction("Index");
-    //        }
-    //        return View();
-    //    }
+        [HttpPost]
+        [ActionName("Create2")]
+        public ActionResult Create2_Post() // No violation
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = new Employee();
+                TryUpdateModel(employee, includeProperties: new[] { "Gender, City, Salary, DateOfBirth" });
+                _db.Employees.Add(employee);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
 
-    //    [HttpPost]
-    //    [ActionName("Create3")]
-    //    public ActionResult Create3_Post() // No violation
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-    //            var employee = new Employee();
-    //            TryUpdateModel(employee, new[] { "Gender, City, Salary, DateOfBirth" });
-    //            _db.Employees.Add(employee);
-    //            _db.SaveChanges();
-    //            return RedirectToAction("Index");
-    //        }
-    //        return View();
-    //    }
+        [HttpPost]
+        [ActionName("Create3")]
+        public ActionResult Create3_Post() // No violation
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = new Employee();
+                TryUpdateModel(employee, new[] { "Gender, City, Salary, DateOfBirth" });
+                _db.Employees.Add(employee);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
 
-    //    [HttpPost]
-    //    [ActionName("Create4")]
-    //    public ActionResult Create4_Post() // No violation
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-    //            var employee = new Employee();
-    //            var includeProperties = new[] { "Gender, City, Salary, DateOfBirth" };
-    //            TryUpdateModel(employee, includeProperties);
-    //            _db.Employees.Add(employee);
-    //            _db.SaveChanges();
-    //            return RedirectToAction("Index");
-    //        }
-    //        return View();
-    //    }
-    //}
+        [HttpPost]
+        [ActionName("Create4")]
+        public ActionResult Create4_Post() // No violation
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = new Employee();
+                var includeProperties = new[] { "Gender, City, Salary, DateOfBirth" };
+                TryUpdateModel(employee, includeProperties);
+                _db.Employees.Add(employee);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
 
-    //public class EmployeeDbcontext : DbContext
-    //{
-    //    public DbSet<Employee> Employees { get; set; }
-    //}
+    public class EmployeeDbcontext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
 
-    //public interface IEmployee
-    //{
-    //    int ID { get; set; }
-    //    string Gender { get; set; }
-    //    string City { get; set; }
-    //    decimal Salary { get; set; }
-    //    DateTime DateOfBirth { get; set; }
-    //}
-    //// Step 2: Make "Employee" class inherit from IEmployee interface
-    //public class Employee : IEmployee
-    //{
-    //    public int ID { get; set; }
-    //    public string Name { get; set; }
-    //    public string Gender { get; set; }
-    //    public string City { get; set; }
-    //    public decimal Salary { get; set; }
-    //    public DateTime DateOfBirth { get; set; }
-    //}
+    public interface IEmployee
+    {
+        int ID { get; set; }
+        string Gender { get; set; }
+        string City { get; set; }
+        decimal Salary { get; set; }
+        DateTime DateOfBirth { get; set; }
+    }
+    // Step 2: Make "Employee" class inherit from IEmployee interface
+    public class Employee : IEmployee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
 
 
 
