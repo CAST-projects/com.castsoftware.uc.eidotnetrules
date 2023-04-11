fa6a104583f1e53b775ccfa8b48988e7ad31ae6f
Ludovic Baillet <l.baillet@castsoftware.com>
2023-04-11 15:17:19 +0200

    EINETRULES-120 - Add test AvoidUsingXPathNavigatorWithoutRestrictionOfXMLExternalEntityReference

---
+++ UnitTests/TestAvoidUsingXPathNavigatorWithoutRestrictionOfXMLExternalEntityReference.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CastDotNetExtension;
+using CastDotNetExtension.Utils;
+
+namespace UnitTests.UnitTest
+{
+    [TestFixture]
+    class TestAvoidUsingXPathNavigatorWithoutRestrictionOfXMLExternalEntityReference
+    {
+        [Test]
+        public void TestNetFramework472()
+        {
+            var testSrc = UnitTests.Properties.SourcesToTest.AvoidUsingXPathNavigatorWithoutRestrictionOfXMLExternalEntityReference_Source;
+
+            var checker = CastDotNetExtensionChecker<AvoidUsingXPathNavigatorWithoutRestrictionOfXMLExternalEntityReference>.CreateInstance();
+            Assert.IsTrue(checker != null);
+
+            // Set current Net Framework
+            FrameworkVersion.currentNetFrameworkKind = NetFrameworkKind.NetFramework;
+            FrameworkVersion.currentNetFrameworkVersion = new Version("4.7.2");
+            checker
+                //.AddAssemblyRef(@"C:\packages\assembly.dll")
+                .Apply(testSrc);
+
+            checker
+                .Validate();
+            Console.WriteLine(checker.GetStatus());
+            Assert.IsTrue(checker.IsValid(), checker.GetStatus());
+
+            //Assert.IsFalse(checker.ResultsMissing.Any());
+            //Assert.IsFalse(checker.ResultsUnexpected.Any());
+
+        }
+
+        [Test]
+        public void TestNetFramework451()
+        {
+            var testSrc = UnitTests.Properties.SourcesToTest.AvoidUsingXPathNavigatorWithoutRestrictionOfXMLExternalEntityReference_Source;
+
+            var checker = CastDotNetExtensionChecker<AvoidUsingXPathNavigatorWithoutRestrictionOfXMLExternalEntityReference>.CreateInstance();
+            Assert.IsTrue(checker != null);
+
+            // Set current Net Framework
+            FrameworkVersion.currentNetFrameworkKind = NetFrameworkKind.NetFramework;
+            FrameworkVersion.currentNetFrameworkVersion = new Version("4.5.1");
+            checker
+                //.AddAssemblyRef(@"C:\packages\assembly.dll")
+                .Apply(testSrc);
+
+            checker
+                .AddExpected(17, 33)
+                .AddExpected(24, 33)
+                .Validate();
+            Console.WriteLine(checker.GetStatus());
+            Assert.IsTrue(checker.IsValid(), checker.GetStatus());
+
+            //Assert.IsFalse(checker.ResultsMissing.Any());
+            //Assert.IsFalse(checker.ResultsUnexpected.Any());
+
+        }
+    }
+}
---
+++ UnitTests/TestSources/AvoidUsingXPathNavigatorWithoutRestrictionOfXMLExternalEntityReference_Source.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
+
+
+namespace UnitTests.TestSources
+{
+    public class AvoidUsingXPathNavigatorWithoutRestrictionOfXMLExternalEntityReference_Source
+    {
+
+        public void function()
+        {
+            XPathDocument doc = new XPathDocument("example.xml");
+            XPathNavigator nav = doc.CreateNavigator();
+            string xml = nav.InnerXml.ToString();
+        }
+
+        public void function2(string filePath)
+        {
+            XPathDocument doc = new XPathDocument(filePath);
+            XPathNavigator nav = doc.CreateNavigator();
+            string xml = nav.InnerXml.ToString();
+        }
+
+        public void function3()
+        {
+            XmlReader reader = XmlReader.Create("example.xml");
+            XPathDocument doc = new XPathDocument(reader);
+            XPathNavigator nav = doc.CreateNavigator();
+            string xml = nav.InnerXml.ToString();
+        }
+    }
+}
