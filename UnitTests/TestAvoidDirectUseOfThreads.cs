e4169794638c3801b4eeeb3fe8f0d0a00f41aece
Ludovic Baillet <l.baillet@castsoftware.com>
2023-04-11 14:22:33 +0200

    EINETRULES-120 - Add test AvoidDirectUseOfThreads

---
+++ UnitTests/TestAvoidDirectUseOfThreads.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CastDotNetExtension;
+
+namespace UnitTests.UnitTest
+{
+    [TestFixture]
+    public class TestAvoidDirectUseOfThreads
+    {
+        [Test]
+        public void Test()
+        {
+            var testSrc = UnitTests.Properties.SourcesToTest.AvoidDirectUseOfThreads_Source;
+
+            var checker = CastDotNetExtensionChecker<AvoidDirectUseOfThreads>.CreateInstance();
+            Assert.IsTrue(checker != null);
+
+
+            checker
+                //.AddAssemblyRef(@"C:\packages\assembly.dll")
+                .Apply(testSrc);
+
+            checker
+               .AddExpected(34, 23)
+               .AddExpected(56, 12)
+                .Validate();
+            Console.WriteLine(checker.GetStatus());
+            Assert.IsTrue(checker.IsValid(), checker.GetStatus());
+
+            //Assert.IsFalse(checker.ResultsMissing.Any());
+            //Assert.IsFalse(checker.ResultsUnexpected.Any());
+
+        }
+    }
+}
---
+++ UnitTests/TestSources/AvoidDirectUseOfThreads_Source.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnitTests.TestSources
+{
+    public class AvoidDirectUseOfThreads_Source
+    {
+        public static void ThreadProc()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                // Yield the rest of the time slice.
+                Thread.Sleep(0);
+            }
+        }
+
+        // This thread procedure performs the task.
+        static void ThreadProc2(Object stateInfo)
+        {
+            // No state object was passed to QueueUserWorkItem, so stateInfo is null.
+            Console.WriteLine("Hello from the thread pool.");
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Main thread: Start a second thread.");
+            // The constructor for the Thread class requires a ThreadStart
+            // delegate that represents the method to be executed on the
+            // thread.  C# simplifies the creation of this delegate.
+            Thread t = new Thread(new ThreadStart(ThreadProc));
+
+            // Start ThreadProc.  Note that on a uniprocessor, the new
+            // thread does not get any processor time until the main thread
+            // is preempted or yields.  Uncomment the Thread.Sleep that
+            // follows t.Start() to see the difference.
+            t.Start();
+            //Thread.Sleep(0);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+
+            Console.WriteLine("Main thread: Call Join(), to wait until ThreadProc ends.");
+            t.Join();
+            Console.WriteLine("Main thread: ThreadProc.Join has returned.  Press Enter to end program.");
+            Console.ReadLine();
+
+
+            // Queue the task.
+            ThreadPool.QueueUserWorkItem(ThreadProc2);
+            Console.WriteLine("Main thread does some work, then sleeps.");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Main thread exits.");
+        }
+    }
+}
