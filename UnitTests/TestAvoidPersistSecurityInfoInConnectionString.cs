3749b33008f1047cdaf178508c43ddeb6fcdd28d
Ludovic Baillet <l.baillet@castsoftware.com>
2023-04-11 14:30:07 +0200

    EINETRULES-120 - Add test AvoidPersistSecurityInfoInConnectionString

---
+++ UnitTests/TestAvoidPersistSecurityInfoInConnectionString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CastDotNetExtension;
+
+namespace UnitTests.UnitTest
+{
+    [TestFixture]
+    class TestAvoidPersistSecurityInfoInConnectionString
+    {
+        [Test]
+        public void TestAvoidPersistSecurityInfoInConnectionString1()
+        {
+            var testSrc = UnitTests.Properties.SourcesToTest.AvoidPersistSecurityInfoInConnectionString_Source;
+
+            var checker = CastDotNetExtensionChecker<AvoidPersistSecurityInfoInConnectionString>.CreateInstance();
+            Assert.IsTrue(checker != null);
+
+
+            checker
+                //.AddAssemblyRef(@"C:\packages\assembly.dll")
+                .Apply(testSrc);
+
+            checker
+               .AddExpected(18, 12)
+               .AddExpected(20, 12)
+               .AddExpected(24, 12)
+               .AddExpected(25, 12)
+               .AddExpected(32, 12)
+               .AddExpected(37, 12)
+               .AddExpected(38, 12)
+               .AddExpected(45, 12)
+               .AddExpected(50, 12)
+               .AddExpected(51, 12)
+                .Validate();
+            Console.WriteLine(checker.GetStatus());
+            Assert.IsTrue(checker.IsValid(), checker.GetStatus());
+
+            //Assert.IsFalse(checker.ResultsMissing.Any());
+            //Assert.IsFalse(checker.ResultsUnexpected.Any());
+
+        }
+    }
+}
---
+++ UnitTests/TestSources/AvoidPersistSecurityInfoInConnectionString_Source.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+
+namespace UnitTests.UnitTest.Sources
+{
+    class AvoidPersistSecurityInfoInConnectionString_Source
+    {
+        public void func()
+        {
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder["Data Source"] = "(local)";
+            builder["Persist Security Info"] = true;     // VIOLATION
+            builder["Initial Catalog"] = "AdventureWorks;NewValue=Bad";
+            builder.PersistSecurityInfo = true;
+            DbConnectionStringBuilder builder2 = new DbConnectionStringBuilder();
+            builder2.ConnectionString = @"Data Source=c:\MyData\MyDb.mdb";
+            builder2.Add("Initial Catalog", "TheDatabase");
+            builder2.Add("Persist Security Info", true);
+            builder2.Add("PersistSecurityInfo", true);
+        }
+
+        public void func2()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder["Data Source"] = "(local)";
+            builder["Persist Security Info"] = "True";     // VIOLATION
+            builder["Initial Catalog"] = "AdventureWorks;NewValue=Bad";
+            DbConnectionStringBuilder builder2 = new DbConnectionStringBuilder();
+            builder2.ConnectionString = @"Data Source=c:\MyData\MyDb.mdb";
+            builder2.Add("Initial Catalog", "TheDatabase");
+            builder2.Add("Persist Security Info", "True");
+            builder2.Add("PersistSecurityInfo", "True");
+        }
+
+        public void func3()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder["Data Source"] = "(local)";
+            builder["Persist Security Info"] = "true";     // VIOLATION
+            builder["Initial Catalog"] = "AdventureWorks;NewValue=Bad";
+            DbConnectionStringBuilder builder2 = new DbConnectionStringBuilder();
+            builder2.ConnectionString = @"Data Source=c:\MyData\MyDb.mdb";
+            builder2.Add("Initial Catalog", "TheDatabase");
+            builder2.Add("Persist Security Info", "true");
+            builder2.Add("PersistSecurityInfo", "true");
+        }
+    }
+}
+
