da3ffef589b0d225d6606d27db4387a51bb9e1c2
Ludovic Baillet <l.baillet@castsoftware.com>
2023-04-11 14:32:36 +0200

    EINETRULES-120 - Add test AvoidSecurityCriticalInformationExposure

---
+++ UnitTests/TestAvoidSecurityCriticalInformationExposure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CastDotNetExtension;
+
+namespace UnitTests.UnitTest
+{
+    [TestFixture]
+    class TestAvoidSecurityCriticalInformationExposure
+    {
+        [Test]
+        public void Test()
+        {
+            var testSrc = UnitTests.Properties.SourcesToTest.AvoidSecurityCriticalInformationExposure_Source;
+
+            var checker = CastDotNetExtensionChecker<AvoidSecurityCriticalInformationExposure>.CreateInstance();
+            Assert.IsTrue(checker != null);
+
+            checker
+                //.AddAssemblyRef(@"C:\packages\assembly.dll")
+                .Apply(testSrc);
+
+            checker
+               .AddExpected(29, 12)
+               .AddExpected(30, 12)
+               .AddExpected(31, 12)
+               .AddExpected(32, 12)
+                .Validate();
+            Console.WriteLine(checker.GetStatus());
+            Assert.IsTrue(checker.IsValid(), checker.GetStatus());
+
+            //Assert.IsFalse(checker.ResultsMissing.Any());
+            //Assert.IsFalse(checker.ResultsUnexpected.Any());
+
+        }
+    }
+}
---
+++ UnitTests/TestSources/AvoidSecurityCriticalInformationExposure_Source.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Diagnostics;
+
+namespace UnitTests.TestSources
+{
+    class AvoidSecurityCriticalInformationExposure_Source
+    {
+        [SecurityCritical]
+        static int x = 10;
+        private string _password = "pass";
+        public string Password
+        {
+            [SecurityCritical]
+            get { return _password; }
+        }
+
+        [SecurityCritical]
+        public string getAccountInfo()
+        {
+            return "account info";
+        }
+
+        public void func(string[] args)
+        {
+            Console.WriteLine(" Critical X " + x); // Violation
+            Console.WriteLine(" Password " + Password); // Violation 
+            Console.WriteLine(" AccountInfo " + getAccountInfo()); // Violation
+            Debug.WriteLine(" Password " + Password); // Violation 
+        }
+    }
+}
