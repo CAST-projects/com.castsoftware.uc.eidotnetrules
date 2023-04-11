1688b7cf93dda660a7fc0b37b90b347c8b5811e2
Ludovic Baillet <l.baillet@castsoftware.com>
2023-04-11 14:58:36 +0200

    EINETRULES-120 - Fix build

--- UnitTests/TestAvoidStoringPasswordInString.cs
+++ UnitTests/TestAvoidStoringPasswordInString.cs
@@ -31,8 +31,8 @@ namespace UnitTests.UnitTest
                .AddExpected(16, 16)
                .AddExpected(23, 16)
                 .Validate();
-            Console.WriteLine(checker.getStatus());
-            Assert.IsTrue(checker.IsValid(), checker.getStatus());
+            Console.WriteLine(checker.GetStatus());
+            Assert.IsTrue(checker.IsValid(), checker.GetStatus());
 
             //Assert.IsFalse(checker.ResultsMissing.Any());
             //Assert.IsFalse(checker.ResultsUnexpected.Any());
