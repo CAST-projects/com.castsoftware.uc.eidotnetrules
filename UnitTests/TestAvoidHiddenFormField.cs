e84b46e29cfc8097852d87180045bec059a44a96
Ludovic Baillet <l.baillet@castsoftware.com>
2023-04-11 14:27:11 +0200

    EINETRULES-120 - Add test AvoidHiddenFormField

---
+++ UnitTests/TestAvoidHiddenFormField.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CastDotNetExtension;
+
+namespace UnitTests.UnitTest
+{
+    [TestFixture]
+    class TestAvoidHiddenFormField
+    {
+        [Test]
+        public void Test()
+        {
+            var testSrc = UnitTests.Properties.SourcesToTest.AvoidHiddenFormField_Source;
+
+            var checker = CastDotNetExtensionChecker<AvoidHiddenFormField>.CreateInstance();
+            Assert.IsTrue(checker != null);
+
+
+            checker
+                //.AddAssemblyRef(@"C:\packages\assembly.dll")
+                .Apply(testSrc);
+
+            checker
+               .AddExpected(13, 37)
+                .Validate();
+            Console.WriteLine(checker.GetStatus());
+            Assert.IsTrue(checker.IsValid(), checker.GetStatus());
+
+            //Assert.IsFalse(checker.ResultsMissing.Any());
+            //Assert.IsFalse(checker.ResultsUnexpected.Any());
+
+        }
+    }
+}
