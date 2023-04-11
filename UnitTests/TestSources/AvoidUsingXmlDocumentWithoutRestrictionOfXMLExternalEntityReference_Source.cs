a4a418c42a6fff3ac48484fc8b409fd902afa56b
Ludovic Baillet <l.baillet@castsoftware.com>
2023-04-11 15:10:14 +0200

    EINETRULES-120 - Add test AvoidUsingXmlDocumentWithoutRestrictionOfXMLExternalEntityReference

---
+++ UnitTests/TestSources/AvoidUsingXmlDocumentWithoutRestrictionOfXMLExternalEntityReference_Source.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace UnitTests.TestSources
+{
+    public class AvoidUsingXmlDocumentWithoutRestrictionOfXMLExternalEntityReference_Source
+    {
+        public void func()
+        {
+            // .NET Framework < 4.5.2
+            XmlDocument parser = new XmlDocument(); // VIOLATION: XmlDocument is not safe by default
+            parser.LoadXml("xxe.xml");
+        }
+
+        public void func2()
+        {
+            // .Net Framework 4.5.1
+            XmlDocument parser = new XmlDocument();
+            parser.XmlResolver = null; // FIXED: XmlResolver has been set to null
+            parser.LoadXml("xxe.xml");
+        }
+
+        public void func3()
+        {
+            XmlDocument parser = new XmlDocument(); // .Net Framework 4.7.2
+            parser.XmlResolver = new XmlUrlResolver(); // VIOLATION: XmlDocument.XmlResolver configured with XmlUrlResolver 
+            parser.LoadXml("xxe.xml");
+        }
+    }
+}
