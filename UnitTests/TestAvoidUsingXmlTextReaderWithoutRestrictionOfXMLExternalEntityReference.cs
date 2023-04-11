d7885679ff41fda1745871e2b66a01952991c020
Ludovic Baillet <l.baillet@castsoftware.com>
2023-04-11 15:14:30 +0200

    EINETRULES-120 - Add test AvoidUsingXmlTextReaderWithoutRestrictionOfXMLExternalEntityReference

---
+++ UnitTests/TestAvoidUsingXmlTextReaderWithoutRestrictionOfXMLExternalEntityReference.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CastDotNetExtension;
+using CastDotNetExtension.Utils;
+
+
+namespace UnitTests.UnitTest
+{
+    [TestFixture]
+    class TestAvoidUsingXmlTextReaderWithoutRestrictionOfXMLExternalEntityReference
+    {
+        [Test]
+        public void TestNetFramework472()
+        {
+            var testSrc = UnitTests.Properties.SourcesToTest.AvoidUsingXmlTextReaderWithoutRestrictionOfXMLExternalEntityReference_Source;
+
+            var checker = CastDotNetExtensionChecker<AvoidUsingXmlTextReaderWithoutRestrictionOfXMLExternalEntityReference>.CreateInstance();
+            Assert.IsTrue(checker != null);
+
+            // Set current Net Framework
+            FrameworkVersion.currentNetFrameworkKind = NetFrameworkKind.NetFramework;
+            FrameworkVersion.currentNetFrameworkVersion = new Version("4.7.2");
+            checker
+                //.AddAssemblyRef(@"C:\packages\assembly.dll")
+                .Apply(testSrc);
+
+            checker
+                .AddExpected(43, 35)
+                .Validate();
+            Console.WriteLine(checker.GetStatus());
+            Assert.IsTrue(checker.IsValid(), checker.GetStatus());
+
+            //Assert.IsFalse(checker.ResultsMissing.Any());
+            //Assert.IsFalse(checker.ResultsUnexpected.Any());
+
+        }
+
+        [Test]
+        public void TestNetFramework451()
+        {
+            var testSrc = UnitTests.Properties.SourcesToTest.AvoidUsingXmlTextReaderWithoutRestrictionOfXMLExternalEntityReference_Source;
+
+            var checker = CastDotNetExtensionChecker<AvoidUsingXmlTextReaderWithoutRestrictionOfXMLExternalEntityReference>.CreateInstance();
+            Assert.IsTrue(checker != null);
+
+            // Set current Net Framework
+            FrameworkVersion.currentNetFrameworkKind = NetFrameworkKind.NetFramework;
+            FrameworkVersion.currentNetFrameworkVersion = new Version("4.5.1");
+            checker
+                //.AddAssemblyRef(@"C:\packages\assembly.dll")
+                .Apply(testSrc);
+
+            checker
+                .AddExpected(14, 35)
+                .AddExpected(43, 35)
+                .Validate();
+            Console.WriteLine(checker.GetStatus());
+            Assert.IsTrue(checker.IsValid(), checker.GetStatus());
+
+            //Assert.IsFalse(checker.ResultsMissing.Any());
+            //Assert.IsFalse(checker.ResultsUnexpected.Any());
+
+        }
+
+        [Test]
+        public void TestNetFramework35()
+        {
+            var testSrc = UnitTests.Properties.SourcesToTest.AvoidUsingXmlTextReaderWithoutRestrictionOfXMLExternalEntityReference_Source;
+
+            var checker = CastDotNetExtensionChecker<AvoidUsingXmlTextReaderWithoutRestrictionOfXMLExternalEntityReference>.CreateInstance();
+            Assert.IsTrue(checker != null);
+
+            // Set current Net Framework
+            FrameworkVersion.currentNetFrameworkKind = NetFrameworkKind.NetFramework;
+            FrameworkVersion.currentNetFrameworkVersion = new Version("3.5");
+            checker
+                //.AddAssemblyRef(@"C:\packages\assembly.dll")
+                .Apply(testSrc);
+
+            checker
+                .AddExpected(14, 35)
+                .AddExpected(43, 35)
+                .Validate();
+            Console.WriteLine(checker.GetStatus());
+            Assert.IsTrue(checker.IsValid(), checker.GetStatus());
+
+            //Assert.IsFalse(checker.ResultsMissing.Any());
+            //Assert.IsFalse(checker.ResultsUnexpected.Any());
+
+        }
+    }
+}
